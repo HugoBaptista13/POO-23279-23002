14aa6a38970bfaedd26ef6e567a173435db345e3
NunoGajoLesi <a23002@alunos.ipca.pt>
2023-12-11 09:37:50 +0000

    recintos Inserir Alterar Remover Existe

--- TrabalhoPOO/GestaoZoo/DadosDLL/Recintos.cs
+++ TrabalhoPOO/GestaoZoo/DadosDLL/Recintos.cs
@@ -84,9 +84,9 @@ namespace DadosDLL
         #endregion
         #region OUTROS
         /// <summary>
-        /// Método para listar as vendas todas
+        /// Método para listar os recintos todos
         /// </summary>
-        /// <param name="output">Array de strings com as vendas</param>
+        /// <param name="output">Array de strings com os recintos</param>
         public void Listar(out string[] output)
         {
             int i = 0;
@@ -98,6 +98,113 @@ namespace DadosDLL
                 output[i] = recinto.Listar();
                 i++;
             }
+        }
+
+        /// <summary>
+        /// Método para adicionar um recinto
+        /// </summary>
+        /// <param name="recinto">Objeto do tipo recinto</param>
+        /// <returns>Se conseguir adicionar retorna true, senão retorna false</returns>
+        public bool Inserir(Recinto recinto)
+        {
+            if (l_recintos == null || recinto == null)
+            {
+                return false;
+            }
+            for (int i = 0; i < MAXRECINTOS; i++)
+            {
+                if (l_recintos[i] == recinto)
+                {
+                    return false;
+                }
+                if (l_recintos[i].Id == -1)
+                {
+                    l_recintos[i].Id = recinto.Id;
+                    l_recintos[i].Nome = recinto.Nome;
+                    l_recintos[i].Tipo = recinto.Tipo;
+                    l_recintos[i].Comprimento = recinto.Comprimento;
+                    l_recintos[i].Largura = recinto.Largura;
+                    l_recintos[i].Altura = recinto.Altura;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Método para alterar os dados de um recinto
+        /// </summary>
+        /// <param name="recinto">Objeto do tipo recinto</param>
+        /// <returns>Se conseguir alterar retorna true, senão retorna false</returns>
+        public bool Alterar(Recinto recinto)
+        {
+            if (recinto == null || l_recintos == null)
+                return false;
+            for (int i = 0; i < MAXRECINTOS; i++)
+            {
+                if (l_recintos[i] != recinto)
+                {
+                    return false;
+                }
+                if (l_recintos[i].Id == recinto.Id)
+                {
+                    l_recintos[i].Id = recinto.Id;
+                    l_recintos[i].Nome = recinto.Nome;
+                    l_recintos[i].Tipo = recinto.Tipo;
+                    l_recintos[i].Comprimento = recinto.Comprimento;
+                    l_recintos[i].Largura = recinto.Largura;
+                    l_recintos[i].Altura = recinto.Altura;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Método para remover os dados de um recinto
+        /// </summary>
+        /// <param name="recinto">Objeto do tipo recinto</param>
+        /// <returns>Se conseguir remover retorna true, senão retorna false</returns>
+        public bool Remover(Recinto recinto)
+        {
+            if (recinto == null || l_recintos == null)
+                return false;
+            for (int i = 0; i < MAXRECINTOS; i++)
+            {
+                if (l_recintos[i] != recinto)
+                {
+                    return false;
+                }
+                if (l_recintos[i].Id == recinto.Id)
+                {
+                    l_recintos[i].Id = -1;
+                    l_recintos[i].Nome = "";
+                    l_recintos[i].Tipo = "";
+                    l_recintos[i].Comprimento = -1;
+                    l_recintos[i].Largura = -1;
+                    l_recintos[i].Altura = -1;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Método para verificar se existe um recinto
+        /// </summary>
+        /// <param name="recinto">Objeto do tipo recinto</param>
+        /// <returns>Se verificar que existe retorna true, senão retorna false</returns>
+        public bool Existe(Recinto recinto)
+        {
+            if (recinto == null || l_recintos == null)
+                return false;
+            for (int i = 0; i < MAXRECINTOS; i++)
+            {
+                if (l_recintos[i] != recinto)
+                {
+                    return false;
+                }
+                if (l_recintos[i].Id == recinto.Id)
+                {
+                    break;
+                }
+            }
+            return true;
         }
         #endregion
         #endregion
