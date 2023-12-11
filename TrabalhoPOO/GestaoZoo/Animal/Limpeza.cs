9f28bb3b1791de27863d011a560cbf7d2abd6ed0
NunoGajoLesi <a23002@alunos.ipca.pt>
2023-12-11 12:58:28 +0000

    ILimpeza

--- TrabalhoPOO/GestaoZoo/Animal/Limpeza.cs
+++ TrabalhoPOO/GestaoZoo/Animal/Limpeza.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterfaceDLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -10,7 +11,7 @@ namespace ZooDLL
     /// <summary>
     /// Classe para descrever uma limpeza
     /// </summary>
-    public class Limpeza
+    public class Limpeza : ILimpeza
     {
         #region ATRIBUTOS
         private int id;
