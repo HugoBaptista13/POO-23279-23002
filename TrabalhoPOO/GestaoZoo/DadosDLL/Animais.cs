09295b463b8088d175ed6f40c2d2e4a3d762a776
NunoGajoLesi <a23002@alunos.ipca.pt>
2023-12-14 15:48:36 +0000

    Alterações Do stor

--- TrabalhoPOO/GestaoZoo/DadosDLL/Animais.cs
+++ TrabalhoPOO/GestaoZoo/DadosDLL/Animais.cs
@@ -1,9 +1,4 @@
-﻿using InterfaceDLL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using ZooDLL;
 
 namespace DadosDLL
