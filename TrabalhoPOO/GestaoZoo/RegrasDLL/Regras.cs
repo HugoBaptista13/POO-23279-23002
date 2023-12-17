57cf3b9f593469bca1c2c78098aeae2b5a6abe40
HugoBaptista13 <a23279@alunos.ipca.pt>
2023-12-17 18:02:51 +0000

    Planeamento das regras de negócio

---
+++ TrabalhoPOO/GestaoZoo/RegrasDLL/Regras.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegrasDLL
+{
+    /// <summary>
+    /// Regras de negócio
+    /// </summary>
+    public class RegrasAnimais
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasFuncionarios
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasEventos
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasBilhete
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasComidas
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasConsultas
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasLimpezas
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasRecintos
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+    public class RegrasVendas
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+
+}
