6a3c7d1e00e3fa50c91bfed14f60cd300084f123
HugoBaptista13 <a23279@alunos.ipca.pt>
2023-12-19 14:47:47 +0000

    Alteração das regras

--- TrabalhoPOO/GestaoZoo/RegrasDLL/RegrasAnimais.cs
+++ TrabalhoPOO/GestaoZoo/RegrasDLL/RegrasAnimais.cs
@@ -177,6 +177,114 @@ namespace RegrasDLL
         }
         #endregion
         #region Alterar
+        public static bool Alterar(Animal animal)
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            try
+            {
+                if (animal == null)
+                    throw new ArgumentNullException("Animal", "Animal não pode ser nulo");
+
+                if (animal.Id <= 0)
+                    throw new InvalidIDException(animal.Id.ToString());
+
+                if (animal.Nome == null)
+                    throw new ArgumentNullException("Nome", "Nome não pode ser nulo");
+
+                if (animal.Idade <= 0)
+                    throw new NegativeNumberException(animal.Idade.ToString());
+
+                if (animal.Sexo == null)
+                    throw new ArgumentNullException("Sexo", "Sexo não pode ser nulo");
+
+                if (animal.Classe == null)
+                    throw new ArgumentNullException("Classe", "Classe não pode ser nulo");
+
+                if (animal.Especie == null)
+                    throw new ArgumentNullException("Especie", "Especie não pode ser nulo");
+
+                if (animal.Dieta == null)
+                    throw new ArgumentNullException("Dieta", "Dieta não pode ser nulo");
+
+                if (animal.DataUltimaConsulta == null)
+                    throw new InvalidDateException();
+
+                if (animal.DataProximaConsulta == null)
+                    throw new InvalidDateException();
+
+                if (animal.Estado == null)
+                    throw new ArgumentNullException("Estado", "Estado não pode ser nulo");
+
+                if (animal.Recinto <= 0)
+                    throw new NegativeNumberException(animal.Recinto.ToString());
+            }
+            catch
+            {
+                return false;
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            try
+            {
+                if (!IsAllLetters(animal.Nome.Trim()) || animal.Nome == string.Empty)
+                    throw new InvalidNameException(animal.Nome.Trim());
+
+                if (!IsAllLetters(animal.Sexo.Trim()) || animal.Sexo == string.Empty)
+                    throw new InvalidTextException(animal.Sexo.Trim());
+
+                if (!IsAllLetters(animal.Classe.Trim()) || animal.Classe == string.Empty)
+                    throw new InvalidTextException(animal.Classe.Trim());
+
+                if (!IsAllLetters(animal.Especie.Trim()) || animal.Especie == string.Empty)
+                    throw new InvalidTextException(animal.Especie.Trim());
+
+                if (!IsAllLetters(animal.Dieta.Trim()) || animal.Dieta == string.Empty)
+                    throw new InvalidTextException(animal.Dieta.Trim());
+
+                if (!IsAllLetters(animal.Estado.Trim()) || animal.Estado == string.Empty)
+                    throw new InvalidTextException(animal.Estado.Trim());
+
+                if (!IsValidState(animal.Estado.Trim()))
+                    throw new InvalidStateException(animal.Estado.Trim());
+
+                if (!IsValidClass(animal.Classe.Trim()))
+                    throw new InvalidClassException(animal.Classe.Trim());
+
+                if (!IsValidDiet(animal.Dieta.Trim()))
+                    throw new InvalidDietException(animal.Dieta.Trim());
+
+                if (!IsValidSex(animal.Sexo.Trim()))
+                    throw new InvalidSexException(animal.Sexo.Trim());
+            }
+            catch
+            {
+                return false;
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            try
+            {
+                if (!Recintos.Existe(animal.Recinto))
+                    throw new DoesNotExistException(animal.Recinto.ToString());
+
+                if (animal.DataUltimaConsulta > animal.DataProximaConsulta)
+                    throw new GreaterThanPreviousDateException(animal.DataUltimaConsulta.ToString(), animal.DataProximaConsulta.ToString());
+
+                if (!Animais.Existe(animal.Id))
+                    throw new DoesNotExistException(animal.Id.ToString());
+            }
+            catch
+            {
+                return false;
+            }
+            if (!Animais.Alterar(animal))
+                return false;
+            return true;
+        }
         #endregion
         #region Remover
         #endregion
@@ -187,24 +295,7 @@ namespace RegrasDLL
         #region Guardar
         #endregion
     }
-    /// <summary>
-    /// Regras de negócio
-    /// </summary>
-    public class RegrasFuncionarios
-    {
-        #region Inserir
-        #endregion
-        #region Alterar
-        #endregion
-        #region Remover
-        #endregion
-        #region Procurar
-        #endregion
-        #region Existe
-        #endregion
-        #region Guardar
-        #endregion
-    }
+    
     /// <summary>
     /// Regras de negócio
     /// </summary>
