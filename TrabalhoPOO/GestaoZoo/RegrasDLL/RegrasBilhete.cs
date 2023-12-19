bccb966bcd40048f50b6543f88f8b117d045c002
NunoGajoLesi <a23002@alunos.ipca.pt>
2023-12-19 16:00:34 +0000

    .

---
+++ TrabalhoPOO/GestaoZoo/RegrasDLL/RegrasBilhete.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegrasDLL
+{
+    /// <summary>
+    /// Regras de negócio
+    /// </summary>
+    public class RegrasBilhete
+    {
+        #region Inserir
+        #endregion
+        #region Alterar
+        #endregion
+        #region Remover
+        #endregion
+        #region Procurar
+        #endregion
+        #region Existe
+        #endregion
+        #region Guardar
+        #endregion
+    }
+
+}
